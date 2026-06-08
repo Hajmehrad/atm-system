4f3162d8b413437f6ad030e51b9ab098c5bbe344
Haj_mehrad <mehrad.mehrad.mehrad1487@gmail.com>
2026-06-08 13:20:13 +0330

    Add __main__.py for direct module execution

---
+++ atm_system/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ATM System - Main Entry Point
+برنامه خودپرداز
+"""
+
+from .atm import atm_system
+
+if __name__ == "__main__":
+    atm_system()
